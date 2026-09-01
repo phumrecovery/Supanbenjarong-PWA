1449d58674cd54dd1a221526e878e4afbe35f47f
Phum Inpanya <phum.inpanya@example.com>
2026-09-01 12:38:34 +0700

    Fix preorder modal total updates

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ import {ApiClient} from "./api.js";
 import {renderPos} from "./pos.js";
 import {renderProduct} from "./product.js";
 import {renderExpense} from "./expense.js?v=expense-v7";
-import {renderPreorder} from "./preorder.js?v=preorder-v5";
+import {renderPreorder} from "./preorder.js?v=preorder-v6";
 
 const api=new ApiClient();
 const main=document.querySelector("#main");
