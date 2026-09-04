ce4925c4d51cfc8c6d75346df6cfffa5141264d1
Phum Inpanya <phum.inpanya@example.com>
2026-09-04 19:44:20 +0700

    Fix settings module browser compatibility

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ import {renderExpense} from "./expense.js?v=expense-v12";
 import {renderPreorder} from "./preorder.js?v=preorder-v13";
 import {renderOutsource} from "./outsource.js?v=outsource-v3";
 import {renderReport} from "./report.js?v=report-v17";
-import {renderSettings} from "./settings.js?v=settings-v1";
+import {renderSettings} from "./settings.js?v=settings-v2";
 
 const api=new ApiClient();
 const main=document.querySelector("#main");
