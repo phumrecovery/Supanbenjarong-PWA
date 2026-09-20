957146ab596088651b5e86bd348e39a0981fcd03
Phum Inpanya <phum.inpanya@example.com>
2026-09-20 20:45:42 +0700

    Clarify monthly report totals

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ import {renderStock} from "./stock.js?v=stock-v4";
 import {renderExpense} from "./expense.js?v=expense-v13";
 import {renderPreorder} from "./preorder.js?v=preorder-v15";
 import {renderOutsource} from "./outsource.js?v=outsource-v3";
-import {renderReport} from "./report.js?v=report-v17";
+import {renderReport} from "./report.js?v=report-v18";
 import {renderSettings} from "./settings.js?v=settings-v10";
 import {renderWorkshop} from "./workshop.js?v=workshop-v24";
 
